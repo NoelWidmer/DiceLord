1dff65e132480a7d2c299b3dc2c243ae17d416ba
Noël Widmer <noel.widmer@gmail.com>
2022-07-16 01:58:57 +0200

    entities can now move logically and visually

--- Assets/Source/Entities/Entity.cs
+++ Assets/Source/Entities/Entity.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public interface IEntity
 {
     GridVector GetCoordinatesFromPosition();
-    void Move();
+    float Move();
     void Attack();
 }
 
@@ -21,8 +21,45 @@ public class Entity : MonoBehaviour, IEntity
         Debug.Log("Attack (from player character)");
     }
 
-    public void Move()
+    private bool _isMoving;
+    private float _remainingMoveDistance;
+
+    public float Move()
     {
         Debug.Log("Move (from player character)");
+        _isMoving = true;
+        _remainingMoveDistance = 1f;
+        enabled = true;
+        return _moveDuration;
+    }
+
+    private float _moveDuration = 2f;
+
+    private void Update()
+    {
+        if (_isMoving)
+        {
+            var distancePerSecond = 1f / _moveDuration;
+            var distanceThisFrame = distancePerSecond * Time.deltaTime;
+
+            if (distanceThisFrame >= _remainingMoveDistance)
+            {
+                distanceThisFrame = _remainingMoveDistance;
+                _remainingMoveDistance = 0f;
+                _isMoving = false;
+                enabled = false;
+            }
+            else
+            {
+                _remainingMoveDistance -= distanceThisFrame;
+            }
+
+            transform.position += Vector3.up * distanceThisFrame;
+
+            if (_isMoving == false)
+            {
+                Grid.Instance.UpdateEntityCoordinates(this, GetCoordinatesFromPosition());
+            }
+        }
     }
 }
