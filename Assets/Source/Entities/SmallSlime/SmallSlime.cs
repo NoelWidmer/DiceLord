09e9558b4d6fc53dd9243b2ac410468bfa98688e
Noël Widmer <noel.widmer@gmail.com>
2022-07-16 21:55:49 +0200

    added interactions for all currently existing characters

---
+++ Assets/Source/Entities/SmallSlime/SmallSlime.cs
@@ -0,0 +1,14 @@
+public class SmallSlime : Entity
+{
+    public override bool CanBeEntered => false;
+    public override bool CanRepell => true;
+
+    public override void OnEntered(IEntity entity)
+    { }
+
+    protected override void OnDirectionalRequest()
+    { }
+
+    protected override void OnDied()
+    { }
+}
