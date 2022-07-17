495d19e456064e42ce3d3138d15b6605644a53c7
Lithimlin <julinksky@googlemail.com>
2022-07-17 18:46:18 +0200

    permanent DiceController

--- Assets/Source/Dice/DiceController.cs
+++ Assets/Source/Dice/DiceController.cs
@@ -5,35 +5,32 @@ using UnityEngine;
 public interface IDiceController
 {
     public List<GameMode.PlayerAction> RollDice(int n);
+    public void SetActions(List<GameMode.PlayerAction> actions);
+    public int GetNumSides();
 }
 
 public class DiceController : Singleton<DiceController, IDiceController>, IDiceController
 {
     public const int NUM_SIDES = 6;
 
-    private GameMode.PlayerAction[] _actions;
+    private List<GameMode.PlayerAction> _actions;
 
     // Start is called before the first frame update
     protected override void OnAwake()
     {
-        _actions = new GameMode.PlayerAction[NUM_SIDES] {
-            GameMode.PlayerAction.Move,
-            GameMode.PlayerAction.Ranged,
-            GameMode.PlayerAction.Melee,
-            GameMode.PlayerAction.Ranged,
-            GameMode.PlayerAction.Melee,
-            GameMode.PlayerAction.Move
-        };
+        _actions = new();
     }
 
-    public void SetActions(GameMode.PlayerAction[] actions)
+    public void SetActions(List<GameMode.PlayerAction> actions)
     {
-        if(actions.Length > NUM_SIDES)
+        if(actions.Count > NUM_SIDES)
         {
-            throw new ArgumentException($"Actions array should have at most {NUM_SIDES} elements, has {actions.Length}!");
+            throw new ArgumentException($"Actions array should have at most {NUM_SIDES} elements, has {actions.Count}!");
         }
         _actions = actions;
     }
+
+    public List<GameMode.PlayerAction> GetDiceActions() => _actions;
 
     public List<GameMode.PlayerAction> RollDice(int n = 1)
     {
@@ -45,9 +42,5 @@ public class DiceController : Singleton<DiceController, IDiceController>, IDiceC
         return rolls;
     }
 
-    // Setters and Getters
-    public int GetNumSides()
-    {
-        return NUM_SIDES;
-    }
+    public int GetNumSides() => NUM_SIDES;
 }
